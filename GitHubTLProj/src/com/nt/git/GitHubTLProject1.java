7f19b1c16ef8739c63e2d2f41c587f340d1a0bd4
bunty <TLBunty@gmail.com>
2017-04-30 22:57:52 +0530

    add to message welcome to github 2

---
+++ GitHubTLProj/src/com/nt/git/GitHubTLProject1.java
@@ -0,0 +1,11 @@
+package com.nt.git;
+
+public class GitHubTLProject1 {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to github 1");
+		System.out.println("welcome to github 2");
+
+	}
+
+}
